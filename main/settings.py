41d1b35260d00e5e84b19fbbdea608f012b5bed2
mngar <matiasngarcia2002@gmail.com>
2024-04-23 10:59:03 -0300

    CORS Headers Error fixed

--- main/settings.py
+++ main/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['dtaforecast.vercel.app']
+ALLOWED_HOSTS = ['dtaforecast.vercel.app', 'apifio.dtalogistica.com']
 
 BASE_APPS = [
     'django.contrib.admin',
